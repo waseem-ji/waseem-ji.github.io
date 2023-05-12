d8dbcb707d41c034a0e0535b5608f72add8fc647
waseem-ji <waseem@mozilor.com>
2023-05-12 17:20:39 +0530

    Added CY script

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("cookieyes_consent_update", function (eventData)
+{
+  const data = eventData.detail;
+//   if (data.accepted.includes("analytics")) {
+//      console.log();
+//    }
+console.log(data);
+ });

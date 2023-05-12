e1adb957b9c85767a3553b9318bba91c0d70f0b1
waseem-ji <waseem@mozilor.com>
2023-05-12 17:31:25 +0530

    Changed URL

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("cookieyes_consent_update", function (eventData) {
   const data = eventData.detail;
   if (data.rejected.includes("analytics")) {
     console.log("rejected");
-    location.href("waseem-ji.github.io/cookiePolicy.html");
+    location.href("https://waseem-ji.github.io/cookiePolicy.html");
   }
   console.log(data);
 });

038e5e9d0ef8ff503289e2bfa61eb06658b34e4e
waseem-ji <waseem@mozilor.com>
2023-05-12 17:38:57 +0530

    BUg fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("cookieyes_consent_update", function (eventData) {
   const data = eventData.detail;
-  if (data.rejected.includes(["analytics","functional"])) {
+  if (data.rejected.includes("analytics", "functional")) {
     console.log("rejected");
     location.href = "https://waseem-ji.github.io/cookiePolicy.html";
   }

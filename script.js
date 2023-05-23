02eaa261f89641311d6adef2c3efc2259d996097
waseem-ji <waseem@mozilor.com>
2023-05-23 14:34:43 +0530

    Removed script

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.addEventListener("cookieyes_consent_update", function (eventData) {
-  const data = eventData.detail;
-  if (data.rejected.includes("analytics", "functional")) {
-    console.log("rejected");
-    location.href = "https://waseem-ji.github.io/cookiePolicy.html";
-  }
-  console.log(data);
-});
+// document.addEventListener("cookieyes_consent_update", function (eventData) {
+//   const data = eventData.detail;
+//   if (data.rejected.includes("analytics", "functional")) {
+//     // location.href = "https://waseem-ji.github.io/cookiePolicy.html";
+//   }
+// });

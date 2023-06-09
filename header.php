a686050358bbc536cac88cd2225911c42e81a83d
waseem-ji <waseem@mozilor.com>
2023-06-09 12:39:57 +0530

    Finished Restructure

---
+++ header.php
@@ -0,0 +1,14 @@
+
+<head>
+<script
+      id="cookieyes"
+      type="text/javascript"
+      src="https://cdn-cookieyes.com/client_data/68f7d3a7fb943675a86b8d43/script.js"
+    ></script>
+    <!-- End cookieyes banner -->
+    <script src="script.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ji's Local Website</title>
+</head>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+require_once 'header.php';
+?>
+  <body>
+    <br />
+    <nav>
+      <a href="firstpage.html">Firstpage</a>
+      <a href="secondpage.html">Second Page</a>
+    </nav>
+
+    <h1>Hey there !!</h1>
+    <h3>Welcome</h3>
+
+    <iframe
+      width="420"
+      height="345"
+      src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1"
+    >
+    </iframe>
+
+    <a href="https://waseem-ji.github.io/cookiePolicy.html"
+      >Our Cookie Policy</a
+    >
+  </body>
+</html>

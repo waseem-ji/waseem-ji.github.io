f8c87d0ddad662f54544e0b479bd4cf24d9a3ad7
waseem-ji <waseem@mozilor.com>
2023-06-09 12:39:13 +0530

    Restructered website toa SPA Made a common header

---
+++ firstpage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+require_once 'header.php';
+?>
+  <body>
+    <br />
+    <nav>
+      <a href="firstpage.html">Firstpage</a>
+      <a href="secondpage.html">Second Page</a>
+    </nav>
+    <br />
+    <h2>This is a sample page</h2>
+    <iframe
+      src="https://player.vimeo.com/video/76979871?h=8272103f6e"
+      width="640"
+      height="360"
+      frameborder="0"
+      allow="autoplay; fullscreen; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <p>
+      <a href="https://vimeo.com/76979871"
+        >The New Vimeo Player (You Know, For Videos)</a
+      >
+      from <a href="https://vimeo.com/staff">Vimeo Staff</a> on
+      <a href="https://vimeo.com">Vimeo</a>.
+    </p>
+
+    <br />
+    <a href="index.html">Go back!</a>
+  </body>
+</html>

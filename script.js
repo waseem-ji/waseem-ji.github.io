44798e8c8ad1301f51e1f9882284ed8e7f2c702a
waseem-ji <waseem@mozilor.com>
2023-06-27 15:24:24 +0530

    Venor restrict

--- script.js
+++ script.js
@@ -1,91 +1,91 @@
-// // Place this code in a script tag or a JavaScript file that is loaded on your website
+// // // Place this code in a script tag or a JavaScript file that is loaded on your website
 
-window.addEventListener("DOMContentLoaded", () => {
-  const cookieName = "cookieyes-analytics";
-  const cookieValue = document.cookie
-    .split(";")
-    .map((cookie) => cookie.trim())
-    .find((cookie) => cookie.startsWith(`${cookieName}=`))
-    .substring(`${cookieName}=`.length);
-
-  console.log(`The value of ${cookieName} is ${cookieValue}`);
-});
-
-document.addEventListener("cookieyes_consent_update", function (eventData) {
-  const data = eventData.detail;
-  if (data.accepted.includes("analytics")) {
-    // perform desired action.
-    console.log(`The analytics cookie is accepted`);
-  } else if (data.rejected.includes("analytics")) {
-    console.log(`The analytics cookie is rejected`);
-  }
-});
-
-// window.addEventListener("load", () => {
+// window.addEventListener("DOMContentLoaded", () => {
+//   const cookieName = "cookieyes-analytics";
 //   const cookieValue = document.cookie
 //     .split(";")
 //     .map((cookie) => cookie.trim())
-//     .find((cookie) => cookie.startsWith("cookieyes-other"));
+//     .find((cookie) => cookie.startsWith(`${cookieName}=`))
+//     .substring(`${cookieName}=`.length);
 
-//   if (cookieValue) {
-//     const cookiePairs = cookieValue
-//       .substring("cookieyes-consent=".length)
-//       .split(",")
-//       .map((pair) => pair.trim());
-//     const cookieObj = {};
+//   console.log(`The value of ${cookieName} is ${cookieValue}`);
+// });
 
-//     cookiePairs.forEach((pair) => {
-//       const [key, value] = pair.split(":");
-//       cookieObj[key] = value;
-//     });
-//     console.log(cookieObj);
-
-//     if (
-//       [
-//         "functional",
-//         "analytics",
-//         "performance",
-//         "advertisement",
-//         "other",
-//       ].every(
-//         (key) => cookieObj[key] === "no" || !cookieObj.hasOwnProperty(key)
-//       )
-//     ) {
-//       console.log(
-//         "The user has opted out of cookies, set strictly necessary cookies only"
-//       );
-//     } else {
-//       if (cookieObj["functional"] === "yes") {
-//         console.log("The user has accepted functional cookies");
-//       } else {
-//         console.log("The user has NOT accepted functional cookies");
-//       }
-
-//       if (cookieObj["analytics"] === "yes") {
-//         console.log("The user has accepted analytics cookies");
-//       } else {
-//         console.log("The user has NOT accepted analytics cookies");
-//       }
-
-//       if (cookieObj["performance"] === "yes") {
-//         console.log("The user has accepted performance cookies");
-//       } else {
-//         console.log("The user has NOT accepted performance cookies");
-//       }
-
-//       if (cookieObj["advertisement"] === "yes") {
-//         console.log("The user has accepted advertisement cookies");
-//       } else {
-//         console.log("The user has NOT accepted advertisement cookies");
-//       }
-
-//       if ("other" in cookieObj) {
-//         if (cookieObj["other"] === "yes") {
-//           console.log("The user has accepted other cookies");
-//         } else {
-//           console.log("The user has NOT accepted other cookies");
-//         }
-//       }
-//     }
+// document.addEventListener("cookieyes_consent_update", function (eventData) {
+//   const data = eventData.detail;
+//   if (data.accepted.includes("analytics")) {
+//     // perform desired action.
+//     console.log(`The analytics cookie is accepted`);
+//   } else if (data.rejected.includes("analytics")) {
+//     console.log(`The analytics cookie is rejected`);
 //   }
 // });
+
+// // window.addEventListener("load", () => {
+// //   const cookieValue = document.cookie
+// //     .split(";")
+// //     .map((cookie) => cookie.trim())
+// //     .find((cookie) => cookie.startsWith("cookieyes-other"));
+
+// //   if (cookieValue) {
+// //     const cookiePairs = cookieValue
+// //       .substring("cookieyes-consent=".length)
+// //       .split(",")
+// //       .map((pair) => pair.trim());
+// //     const cookieObj = {};
+
+// //     cookiePairs.forEach((pair) => {
+// //       const [key, value] = pair.split(":");
+// //       cookieObj[key] = value;
+// //     });
+// //     console.log(cookieObj);
+
+// //     if (
+// //       [
+// //         "functional",
+// //         "analytics",
+// //         "performance",
+// //         "advertisement",
+// //         "other",
+// //       ].every(
+// //         (key) => cookieObj[key] === "no" || !cookieObj.hasOwnProperty(key)
+// //       )
+// //     ) {
+// //       console.log(
+// //         "The user has opted out of cookies, set strictly necessary cookies only"
+// //       );
+// //     } else {
+// //       if (cookieObj["functional"] === "yes") {
+// //         console.log("The user has accepted functional cookies");
+// //       } else {
+// //         console.log("The user has NOT accepted functional cookies");
+// //       }
+
+// //       if (cookieObj["analytics"] === "yes") {
+// //         console.log("The user has accepted analytics cookies");
+// //       } else {
+// //         console.log("The user has NOT accepted analytics cookies");
+// //       }
+
+// //       if (cookieObj["performance"] === "yes") {
+// //         console.log("The user has accepted performance cookies");
+// //       } else {
+// //         console.log("The user has NOT accepted performance cookies");
+// //       }
+
+// //       if (cookieObj["advertisement"] === "yes") {
+// //         console.log("The user has accepted advertisement cookies");
+// //       } else {
+// //         console.log("The user has NOT accepted advertisement cookies");
+// //       }
+
+// //       if ("other" in cookieObj) {
+// //         if (cookieObj["other"] === "yes") {
+// //           console.log("The user has accepted other cookies");
+// //         } else {
+// //           console.log("The user has NOT accepted other cookies");
+// //         }
+// //       }
+// //     }
+// //   }
+// // });

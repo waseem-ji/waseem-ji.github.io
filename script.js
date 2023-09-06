0c0821e54f534b2d2305686d1af4286512ca29da
waseem-ji <waseem@mozilor.com>
2023-09-06 11:21:51 +0530

    Added cookieyes script

--- script.js
+++ script.js
@@ -112,3 +112,10 @@
 // //     }, 500);
 // //   }
 // // }
+
+document.addEventListener("cookieyes_consent_update", function (eventData) {
+  const data = eventData.detail;
+  if (data.accepted.includes("analytics")) {
+    console.log("Include analytics");
+  }
+});
